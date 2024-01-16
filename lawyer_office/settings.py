da8457ba2031a788ec0fc92b33a3f75a3a6d7421
unknown <10119051@mu.edu.lb>
2024-01-16 18:37:00 +0200

    scan appear!

--- lawyer_office/settings.py
+++ lawyer_office/settings.py
@@ -125,7 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 from dotenv import load_dotenv
 
 # project_folder = '/path/to/your/project'
